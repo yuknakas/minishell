0d7561705adfcabf73a72274b3e2958dd6876568
Razak Osmonaliev <razakosmonaliev@Razaks-MacBook-Pro.local>
2025-07-26 12:44:11 +0900

    Reconstructure ptroject tree all files of project saved in src in subject saved information of project

--- libft/lst/ft_lstadd_back_bonus.c
+++ libft/lst/ft_lstadd_back_bonus.c
@@ -12,17 +12,17 @@
 
 #include "../header/libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
 	t_list	*last;
 
-	if (lst == NULL || new == NULL)
+	if (lst == NULL || new_node == NULL)
 		return ;
 	if (*lst == NULL)
 	{
-		*lst = new;
+		*lst = new_node;
 		return ;
 	}
 	last = ft_lstlast(*lst);
-	last->next = new;
+	last->next = new_node;
 }
--- libft/lst/ft_lstadd_front_bonus.c
+++ libft/lst/ft_lstadd_front_bonus.c
@@ -12,10 +12,10 @@
 
 #include "../header/libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new_node)
 {
-	if (new == NULL || lst == NULL)
+	if (new_node == NULL || lst == NULL)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	new_node->next = *lst;
+	*lst = new_node;
 }
