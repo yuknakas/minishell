4005015578ed5ec03766d2a3981a82c5935b384f
Razak Osmonaliev <raosmona@c5r4s1.42tokyo.jp>
2025-08-21 13:29:21 +0900

    heredoc

--- src/exec/exec_heredoc.c
+++ src/exec/exec_heredoc.c
@@ -3,14 +3,98 @@
 /*                                                        :::      ::::::::   */
 /*   exec_heredoc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yuknakas <yuknakas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: raosmona <raosmona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 18:06:41 by raosmona          #+#    #+#             */
-/*   Updated: 2025/08/19 18:11:01 by yuknakas         ###   ########.fr       */
+/*   Updated: 2025/08/21 13:28:49 by raosmona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+// static void	write_heredoc_line(char *line, int fd, t_redir *r,
+//		t_minishell *sh)
+// {
+// 	char	*expanded;
+
+// 	if (!r->no_expand)
+// 	{
+// 		expanded = expand_vars_simple(line, sh);
+// 		if (expanded)
+// 		{
+// 			write(fd, expanded, ft_strlen(expanded));
+// 			free(expanded);
+// 		}
+// 		else
+// 			write(fd, line, ft_strlen(line));
+// 	}
+// 	else
+// 		write(fd, line, ft_strlen(line));
+// 	write(fd, "\n", 1);
+// }
+
+// static void	heredoc_readline_loop(int fd, t_redir *redir, t_minishell *sh)
+// {
+// 	char	*line;
+
+// 	while (1)
+// 	{
+// 		line = readline("> ");
+// 		if (g_sig)
+// 		{
+// 			free(line);
+// 			break ;
+// 		}
+// 		if (line == NULL || ft_strcmp(line, redir->file) == 0)
+// 		{
+// 			free(line);
+// 			break ;
+// 		}
+// 		write_heredoc_line(line, fd, redir, sh);
+// 		free(line);
+// 	}
+// }
+
+// static char	*heredoc_name(t_cmd *cmd)
+// {
+// 	char	*cmd_nb;
+// 	char	*name;
+// 	char	*nb_xx;
+
+// 	cmd_nb = ft_itoa(cmd->cmd_no);
+// 	nb_xx = ft_strjoin(cmd_nb, "_XXXXXX");
+// 	name = ft_strjoin("/tmp/minishell_heredoc_", nb_xx);
+// 	free(cmd_nb);
+// 	free(nb_xx);
+// 	return (name);
+// }
+
+// int	handle_heredoc(t_cmd *cmd, t_redir *redir, t_minishell *sh)
+// {
+// 	int		fd;
+// 	char	*tmp_file;
+
+// 	tmp_file = ft_strdup(heredoc_name(cmd));
+// 	if (tmp_file == NULL)
+// 		return (-1);
+// 	fd = mkstemp(tmp_file);
+// 	if (fd == -1)
+// 		return (free(tmp_file), -2);
+// 	heredoc_readline_loop(fd, redir, sh);
+// 	close(fd);
+// 	fd = open(tmp_file, O_RDONLY);
+// 	free(redir->file);
+// 	redir->file = tmp_file;
+// 	return (fd);
+// }
+
+// This handler is for the dedicated heredoc child process.
+// It simply exits the child, which the parent will detect.
+static void	heredoc_child_sigint(int sig)
+{
+	(void)sig;
+	exit(130);
+}
 
 static void	write_heredoc_line(char *line, int fd, t_redir *r, t_minishell *sh)
 {
@@ -32,18 +116,17 @@ static void	write_heredoc_line(char *line, int fd, t_redir *r, t_minishell *sh)
 	write(fd, "\n", 1);
 }
 
-static void	heredoc_readline_loop(int fd, t_redir *redir, t_minishell *sh)
+// This function now runs inside a dedicated child process.
+static void	heredoc_read_loop(int fd, t_redir *redir, t_minishell *sh)
 {
 	char	*line;
 
+	signal(SIGINT, heredoc_child_sigint);
 	while (1)
 	{
 		line = readline("> ");
-		if (g_sig)
-		{
-			free(line);
+		if (line == NULL)
 			break ;
-		}
 		if (line == NULL || ft_strcmp(line, redir->file) == 0)
 		{
 			free(line);
@@ -54,6 +137,7 @@ static void	heredoc_readline_loop(int fd, t_redir *redir, t_minishell *sh)
 	}
 }
 
+// This function is a placeholder for your existing heredoc_name logic
 static char	*heredoc_name(t_cmd *cmd)
 {
 	char	*cmd_nb;
@@ -72,15 +156,43 @@ int	handle_heredoc(t_cmd *cmd, t_redir *redir, t_minishell *sh)
 {
 	int		fd;
 	char	*tmp_file;
+	pid_t	pid;
+	int		status;
 
-	tmp_file = ft_strdup(heredoc_name(cmd));
-	if (tmp_file == NULL)
+	tmp_file = heredoc_name(cmd);
+	if (!tmp_file)
 		return (-1);
 	fd = mkstemp(tmp_file);
 	if (fd == -1)
 		return (free(tmp_file), -2);
-	heredoc_readline_loop(fd, redir, sh);
+	pid = fork();
+	if (pid == -1)
+		return (close(fd), free(tmp_file), -1);
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_IGN);
+		heredoc_read_loop(fd, redir, sh);
+		close(fd);
+		exit(0);
+	}
 	close(fd);
+	signal(SIGINT, SIG_IGN);
+	waitpid(pid, &status, 0);
+	set_signal_handlers();
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
+	{
+		unlink(tmp_file);
+		free(tmp_file);
+		free(redir->file);
+		redir->file = NULL;
+		write(STDOUT_FILENO, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+		sh->last_status = 130;
+		return (-1);
+	}
 	fd = open(tmp_file, O_RDONLY);
 	free(redir->file);
 	redir->file = tmp_file;
--- src/utils/main_loop.c
+++ src/utils/main_loop.c
@@ -3,15 +3,81 @@
 /*                                                        :::      ::::::::   */
 /*   main_loop.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yuknakas <yuknakas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: raosmona <raosmona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 18:03:42 by raosmona          #+#    #+#             */
-/*   Updated: 2025/08/19 18:11:33 by yuknakas         ###   ########.fr       */
+/*   Updated: 2025/08/21 13:28:35 by raosmona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include <signal.h>
+
+// static int	do_heredoc(t_cmd *cmds, t_minishell *sh)
+// {
+// 	t_redir	*rd;
+// 	int		result;
+
+// 	result = -100;
+// 	while (cmds != NULL)
+// 	{
+// 		rd = cmds->rd;
+// 		while (rd != NULL)
+// 		{
+// 			if (rd->kind == REDIR_HEREDOC)
+// 				result = handle_heredoc(cmds, rd, sh);
+// 			rd = rd->next;
+// 		}
+// 		cmds = cmds->next;
+// 	}
+// 	return (1);
+// }
+
+// static void	process_line_part(char *part, t_minishell *sh)
+// {
+// 	sh->tokens = make_token_list(part);
+// 	if (!sh->tokens)
+// 	{
+// 		sh->last_status = 0;
+// 		return ;
+// 	}
+// 	sh->cmds = parse(sh->tokens, sh);
+// 	if (!sh->cmds)
+// 	{
+// 		free_token_list(sh->tokens);
+// 		return ;
+// 	}
+// 	do_heredoc(sh->cmds, sh);
+// 	sh->last_status = execute_pipeline(sh->cmds, sh);
+// 	free_token_list(sh->tokens);
+// 	free_cmd_list(sh->cmds);
+// }
+
+// void	shell_loop(t_minishell *sh)
+// {
+// 	char	*line;
+// 	char	**parts;
+// 	int		i;
+
+// 	while (1)
+// 	{
+// 		line = readline("minishell$ ");
+// 		if (!line)
+// 			break ;
+// 		add_history(line);
+// 		parts = split_unquoted_semicolons(line);
+// 		free(line);
+// 		if (!parts)
+// 			continue ;
+// 		i = 0;
+// 		while (parts[i])
+// 		{
+// 			process_line_part(parts[i], sh);
+// 			i++;
+// 		}
+// 		_freearr(parts);
+// 	}
+// }
 
 static int	do_heredoc(t_cmd *cmds, t_minishell *sh)
 {
@@ -25,7 +91,10 @@ static int	do_heredoc(t_cmd *cmds, t_minishell *sh)
 		while (rd != NULL)
 		{
 			if (rd->kind == REDIR_HEREDOC)
-				result = handle_heredoc(cmds, rd, sh);
+			{
+				if (handle_heredoc(cmds, rd, sh) == -1)
+					return (-1);
+			}
 			rd = rd->next;
 		}
 		cmds = cmds->next;
@@ -47,7 +116,12 @@ static void	process_line_part(char *part, t_minishell *sh)
 		free_token_list(sh->tokens);
 		return ;
 	}
-	do_heredoc(sh->cmds, sh);
+	if (do_heredoc(sh->cmds, sh) == -1)
+	{
+		free_token_list(sh->tokens);
+		free_cmd_list(sh->cmds);
+		return ;
+	}
 	sh->last_status = execute_pipeline(sh->cmds, sh);
 	free_token_list(sh->tokens);
 	free_cmd_list(sh->cmds);
